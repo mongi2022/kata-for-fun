85f11ae64c2f0c54e4bfe442e1ffd7bdf838e2e7
mongi2022 <mongithabet@tekniva.net>
2023-06-06 15:32:40 +0200

    code fait fonctionner test division par 5

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
@@ -11,13 +11,17 @@ class KataForFunService {
         if (inputNumber % 3 == 0 )
             result+="Kata"
 
+        if (inputNumber % 5 == 0 )
+            result+="For"
 
         for (i in inputNumber.toString()) {
 
-       
+            if (i=='5' )
+                result+="For"
             if (i=='3')
                 result+="Kata"
         }
+
 
         return result.ifEmpty { inputNumber.toString() }
 
